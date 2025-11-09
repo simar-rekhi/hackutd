712c24481294d8dcbc714ad84ea6ca24736a14cb
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2025-11-08 18:47:46 -0600

    Made a basic python backend

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/index')
+def index():
+    return render_template('Home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
